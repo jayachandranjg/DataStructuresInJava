5a3e72a0404355582eeb064185c8eca42a111da9
Jayachandran <jayachandranjg@gmail.com>
2021-07-13 14:03:28 +0530

    Interview code for KGS

---
+++ src/main/java/com/chandran/interviewPrg/ReverseStr.java
@@ -0,0 +1,16 @@
+package com.chandran.interviewPrg;
+
+public class ReverseStr {
+
+	public static void main(String[] args) {
+		String str = "Genpact India ";
+		int len = str.length();
+		char[] chrArr = str.toCharArray();
+		char[] revChrArr = new char[len];
+		for (int idx = 0; idx < len; idx++) {
+			revChrArr[idx] = chrArr[(len-1)-idx];
+		}
+		System.out.println(revChrArr.toString());
+	}
+
+}
